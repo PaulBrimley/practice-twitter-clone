4b24a15bec3258f89a0bdd37c5bd4a01b2ef458a
PaulBrimley <paul.k.brimley@gmail.com>
2015-12-11 13:04:45 -0700

    finished through step 4

--- js/animations.js
+++ js/animations.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
-
+	
 	$('#tweet-controls').addClass('hidden');
 	
-	$('.tweet-compose').click(function() {
+	$('.tweet-compose').focus(function() {
 		
-		$(this).css('height', '200%');
+		$(this).css('height', '4em');
 		$('#tweet-controls').removeClass('hidden');
 		var maxLength = 140;
 		$(this).keyup(function() {
 			var tweetLength = maxLength - $('.tweet-compose').val().length;
 			$('#char-count span').text(tweetLength);
-			
+			if (tweetLength <= 10) {
+				$('#char-count span').addClass('red');
+			} else {
+				$('#char-count span').removeClass('red');
+			}
+
+			if ($('.tweet-compose').val().length >= maxLength) {
+				$("#tweet-submit").prop('disabled', true);
+			} else if ($('.tweet-compose').val().length < maxLength) {
+				$("#tweet-submit").prop('disabled', false);
+			}
 		});
 	});
+
+	$('.tweet-compose').blur(function() {
+		if ($('.tweet-compose').val().length === 0) {
+			$('.tweet-compose').css('height', '2.5em');
+		}
+	});
+	
 	
 
 
@@ -23,3 +40,6 @@ $(document).ready(function() {
 
 
 })
+
+
+/*adsfjgadjgfjgadsjfgadgfakfhgakjhsdgfhagdhfgagfkagdkfjhgadskjfhgakjhsdgfkjhgadskjfhgakjdhgfkjahgdkfjhgajskdhfgakjdsgfkjahgsdfkjhagdfkjga*/
